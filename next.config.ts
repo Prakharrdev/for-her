cd4251fb8ce4edeb997ae890b363accf3dec5282
Prakharrdev <prakhar06.work@gmail.com>
2026-02-14 14:55:48 +0530

    host changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'perkkk.dev',
+          },
+        ],
+        destination: 'https://sakura.perkkk.dev/:path*',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
